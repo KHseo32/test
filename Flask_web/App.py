36547230b3c046caf810b81c0be2c0af27d0de79
KHseo32 <163965038+KHseo32@users.noreply.github.com>
2024-04-08 14:44:47 +0900

    d

--- Flask_web/App.py
+++ Flask_web/App.py
@@ -1,6 +1,6 @@
 ## 기본적인 웹서버 설정
 ## flask 웹프레임워크를 로드
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 ## Flask라는 Class 생성
 ## 생성자 함수 필수 인자 : 파일의 이름(app.py)
@@ -13,13 +13,26 @@ app = Flask(__name__)
 def index():
     # 문자열을 return 하는 것이 아니라 html 문서를 리턴
     # return "Hello World"
+    # render_template : templates 폴더 안에 있는 html
     return render_template('index.html')
 
 ## 주소 생성
 ## localhost : 5000/second
 @app.route('/second')
 def second():
-    return 'Second Page'
+    # return 'Second Page'
+    return render_template('login.html')
+
+## login 주소 생성
+## 로그인 정보(request 메시지)를 받아오는 주소
+@app.route('/login')
+def login():
+    # 해당 주소로 요청이 들어왔을 때(유저가 보낸 데이터가 포함)
+    ## request.args : 유저가 서버에게 get 방식으로 보낸 데이터가 저장되어 있는 공간
+    req = request.args
+    print(req)
+    return ''
+
 
 ## Flask Class 안에 있는 (웹서버 구동)함수 호출
-app.run()
+app.run(debug=True)
